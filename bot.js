582b4bee4f3520f9a84c2f2921870595c9cf25a7
Tanmay2k3 <41178950+Tanmay2k3@users.noreply.github.com>
2019-04-09 19:18:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,32 +5,28 @@ const settings = require('./settings.json');
 let embed = new RichEmbed();
 
 bot.on('ready', () => {
-
-    var botinv = `https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=8`;
-    console.log(`Connected to Discord! Invite me here : ${botinv}`);
+    console.log(`Connected to Discord! Invite me here : https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=8`);
     bot.user.setStatus('available')
     bot.user.setPresence({
         game: {
             name: 'Confessions',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
+            type: 'STREAMING',
+            url: 'https://www.twitch.tv/monstercat'
         }
     });
 
     bot.on('message', async msg => {
-        if (msg.channel.id === settings.channelid){
+        if (msg.channel.id !== settings.channelid) return;
+        if (msg.author.bot) return;
 
-            if (msg.author.id === `${bot.user.id}`){
-            }else{
         msg.delete();
 
         embed = new RichEmbed();
         embed.setColor(16712480);
-        embed.setTitle(`${msg.content}`);
+        embed.setTitle(msg.content);
         embed.setDescription('These confessions are completely anonymous and no record is kept');
-
-        msg.channel.send(embed);} }
-            });
+        msg.channel.send(embed);
+    });
 });
 
 bot.on('error', err => {
